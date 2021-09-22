75c08178052df0fba43393d49e0713886808cac9
zhangjingbohao <zhangjingbohao123@163.com>
2021-09-22 20:51:25 +0800

    update

--- third/main.go
+++ third/main.go
@@ -2,62 +2,40 @@ package main
 
 import (
 	"context"
-	"github.com/pkg/errors"
+	"fmt"
 	"golang.org/x/sync/errgroup"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 )
 
 func main() {
-	http.HandleFunc("/", handleTest)
-
 	server := http.Server{
-		Addr: "addr",
+		Addr: ":80",
 	}
-	go func() {
-		if err := server.ListenAndServe(); err != nil {
-			panic(err)
-		}
-	}()
+	g, ctx := errgroup.WithContext(context.Background())
+	g.Go(func() error {
+		return server.ListenAndServe()
+	})
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
-	for {
-		s := <-c
-		switch s {
-		case syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT:
-			ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-			if err := server.Shutdown(ctx); err != nil {
-				log.Panicln("main.Shutdown")
+	g.Go(func() error {
+		for {
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			case <-c:
+				fmt.Println("exit !!")
+				return server.Shutdown(ctx)
 			}
-			cancel()
-			return
-		default:
-			return
 		}
-	}
-}
-
-func handleTest(w http.ResponseWriter, r *http.Request) {
-
-	g, _ := errgroup.WithContext(context.Background())
-	g.Go(func() error {
-		return errors.New("1")
 	})
-	g.Go(func() error {
-		return errors.New("2")
-	})
-
 	if err := g.Wait(); err != nil {
-		log.Println("handleTest.Wait", err)
-	}
-	_, err := w.Write([]byte("response"))
-	if err != nil {
-		log.Println("handleTest.Write", err)
+		fmt.Println("end !", err)
+		log.Fatal(err)
 	}
 }
 
