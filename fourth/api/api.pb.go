d6bf22c933490f14f2b3c5906de5976287aecd0f
zhangjingbohao <zhangjingbohao123@163.com>
2021-09-26 22:23:06 +0800

    update

--- fourth/api/api.pb.go
+++ fourth/api/api.pb.go
@@ -11,7 +11,6 @@ import (
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
-	emptypb "google.golang.org/protobuf/types/known/emptypb"
 	io "io"
 	math "math"
 	math_bits "math/bits"
@@ -67,26 +66,64 @@ func (m *HelloReq) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_HelloReq proto.InternalMessageInfo
 
+type HelloResp struct {
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *HelloResp) Reset()         { *m = HelloResp{} }
+func (m *HelloResp) String() string { return proto.CompactTextString(m) }
+func (*HelloResp) ProtoMessage()    {}
+func (*HelloResp) Descriptor() ([]byte, []int) {
+	return fileDescriptor_00212fb1f9d3bf1c, []int{1}
+}
+func (m *HelloResp) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *HelloResp) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_HelloResp.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *HelloResp) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_HelloResp.Merge(m, src)
+}
+func (m *HelloResp) XXX_Size() int {
+	return m.Size()
+}
+func (m *HelloResp) XXX_DiscardUnknown() {
+	xxx_messageInfo_HelloResp.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_HelloResp proto.InternalMessageInfo
+
 func init() {
 	proto.RegisterType((*HelloReq)(nil), "demo.v1.HelloReq")
+	proto.RegisterType((*HelloResp)(nil), "demo.v1.HelloResp")
 }
 
 func init() { proto.RegisterFile("api.proto", fileDescriptor_00212fb1f9d3bf1c) }
 
 var fileDescriptor_00212fb1f9d3bf1c = []byte{
-	// 187 bytes of a gzipped FileDescriptorProto
+	// 158 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0x4c, 0x2c, 0xc8, 0xd4,
 	0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0x4f, 0x49, 0xcd, 0xcd, 0xd7, 0x2b, 0x33, 0x94, 0xd2,
 	0x4d, 0xcf, 0x2c, 0xc9, 0x28, 0x4d, 0xd2, 0x4b, 0xce, 0xcf, 0xd5, 0x4f, 0xcf, 0x4f, 0xcf, 0xd7,
-	0x07, 0xcb, 0x27, 0x95, 0xa6, 0x81, 0x79, 0x60, 0x0e, 0x98, 0x05, 0xd1, 0x27, 0x25, 0x9d, 0x9e,
-	0x9f, 0x9f, 0x9e, 0x93, 0x8a, 0x50, 0x95, 0x9a, 0x5b, 0x50, 0x52, 0x09, 0x91, 0x54, 0xe2, 0xe2,
-	0xe2, 0xf0, 0x48, 0xcd, 0xc9, 0xc9, 0x0f, 0x4a, 0x2d, 0x34, 0x2a, 0xe5, 0x62, 0x71, 0x49, 0xcd,
-	0xcd, 0x17, 0x32, 0xe3, 0x62, 0x09, 0xc8, 0xcc, 0x4b, 0x17, 0x12, 0xd3, 0x83, 0xe8, 0xd4, 0x83,
-	0xe9, 0xd4, 0x73, 0x05, 0xe9, 0x94, 0xc2, 0x21, 0x2e, 0x64, 0xca, 0xc5, 0x11, 0x9c, 0x58, 0x09,
-	0x36, 0x4e, 0x48, 0x50, 0x0f, 0xea, 0x5a, 0x3d, 0x98, 0xf1, 0xb8, 0xb4, 0x39, 0x49, 0x9e, 0x78,
-	0x28, 0xc7, 0x70, 0xe2, 0x91, 0x1c, 0xe3, 0x85, 0x47, 0x72, 0x8c, 0x0f, 0x1e, 0xc9, 0x31, 0xce,
-	0x78, 0x2c, 0xc7, 0x10, 0xc5, 0x9c, 0x58, 0x90, 0x99, 0xc4, 0x06, 0x56, 0x6a, 0x0c, 0x08, 0x00,
-	0x00, 0xff, 0xff, 0xc8, 0x09, 0xfa, 0xea, 0x06, 0x01, 0x00, 0x00,
+	0x07, 0xcb, 0x27, 0x95, 0xa6, 0x81, 0x79, 0x60, 0x0e, 0x98, 0x05, 0xd1, 0xa7, 0xc4, 0xc5, 0xc5,
+	0xe1, 0x91, 0x9a, 0x93, 0x93, 0x1f, 0x94, 0x5a, 0xa8, 0xc4, 0xcd, 0xc5, 0x09, 0x65, 0x17, 0x17,
+	0x18, 0x59, 0x72, 0xb1, 0xb8, 0xa4, 0xe6, 0xe6, 0x0b, 0x19, 0x72, 0x71, 0x04, 0x27, 0x56, 0x82,
+	0xc5, 0x85, 0x04, 0xf5, 0xa0, 0xb6, 0xe8, 0xc1, 0xf4, 0x48, 0x09, 0xa1, 0x0b, 0x15, 0x17, 0x38,
+	0x49, 0x9e, 0x78, 0x28, 0xc7, 0x70, 0xe2, 0x91, 0x1c, 0xe3, 0x85, 0x47, 0x72, 0x8c, 0x0f, 0x1e,
+	0xc9, 0x31, 0xce, 0x78, 0x2c, 0xc7, 0x10, 0xc5, 0x9c, 0x58, 0x90, 0x99, 0xc4, 0x06, 0xb6, 0xd5,
+	0x18, 0x10, 0x00, 0x00, 0xff, 0xff, 0x0e, 0x4c, 0xe4, 0x57, 0xba, 0x00, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -101,8 +138,7 @@ const _ = grpc.SupportPackageIsVersion4
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type DemoClient interface {
-	Ping(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*emptypb.Empty, error)
-	SayHello(ctx context.Context, in *HelloReq, opts ...grpc.CallOption) (*emptypb.Empty, error)
+	SayHello(ctx context.Context, in *HelloReq, opts ...grpc.CallOption) (*HelloResp, error)
 }
 
 type demoClient struct {
@@ -113,17 +149,8 @@ func NewDemoClient(cc *grpc.ClientConn) DemoClient {
 	return &demoClient{cc}
 }
 
-func (c *demoClient) Ping(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*emptypb.Empty, error) {
-	out := new(emptypb.Empty)
-	err := c.cc.Invoke(ctx, "/demo.v1.Demo/Ping", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-func (c *demoClient) SayHello(ctx context.Context, in *HelloReq, opts ...grpc.CallOption) (*emptypb.Empty, error) {
-	out := new(emptypb.Empty)
+func (c *demoClient) SayHello(ctx context.Context, in *HelloReq, opts ...grpc.CallOption) (*HelloResp, error) {
+	out := new(HelloResp)
 	err := c.cc.Invoke(ctx, "/demo.v1.Demo/SayHello", in, out, opts...)
 	if err != nil {
 		return nil, err
@@ -133,41 +160,19 @@ func (c *demoClient) SayHello(ctx context.Context, in *HelloReq, opts ...grpc.Ca
 
 // DemoServer is the server API for Demo service.
 type DemoServer interface {
-	Ping(context.Context, *emptypb.Empty) (*emptypb.Empty, error)
-	SayHello(context.Context, *HelloReq) (*emptypb.Empty, error)
+	SayHello(context.Context, *HelloReq) (*HelloResp, error)
 }
 
 // UnimplementedDemoServer can be embedded to have forward compatible implementations.
 type UnimplementedDemoServer struct {
 }
 
-func (*UnimplementedDemoServer) Ping(ctx context.Context, req *emptypb.Empty) (*emptypb.Empty, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Ping not implemented")
-}
-func (*UnimplementedDemoServer) SayHello(ctx context.Context, req *HelloReq) (*emptypb.Empty, error) {
+func (*UnimplementedDemoServer) SayHello(ctx context.Context, req *HelloReq) (*HelloResp, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method SayHello not implemented")
 }
 
 func RegisterDemoServer(s *grpc.Server, srv DemoServer) {
 	s.RegisterService(&_Demo_serviceDesc, srv)
-}
-
-func _Demo_Ping_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(emptypb.Empty)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(DemoServer).Ping(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/demo.v1.Demo/Ping",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(DemoServer).Ping(ctx, req.(*emptypb.Empty))
-	}
-	return interceptor(ctx, in, info, handler)
 }
 
 func _Demo_SayHello_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
@@ -192,10 +197,6 @@ var _Demo_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "demo.v1.Demo",
 	HandlerType: (*DemoServer)(nil),
 	Methods: []grpc.MethodDesc{
-		{
-			MethodName: "Ping",
-			Handler:    _Demo_Ping_Handler,
-		},
 		{
 			MethodName: "SayHello",
 			Handler:    _Demo_SayHello_Handler,
@@ -232,6 +233,33 @@ func (m *HelloReq) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	return len(dAtA) - i, nil
 }
 
+func (m *HelloResp) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *HelloResp) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *HelloResp) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if m.XXX_unrecognized != nil {
+		i -= len(m.XXX_unrecognized)
+		copy(dAtA[i:], m.XXX_unrecognized)
+	}
+	return len(dAtA) - i, nil
+}
+
 func encodeVarintApi(dAtA []byte, offset int, v uint64) int {
 	offset -= sovApi(v)
 	base := offset
@@ -244,6 +272,18 @@ func encodeVarintApi(dAtA []byte, offset int, v uint64) int {
 	return base
 }
 func (m *HelloReq) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	if m.XXX_unrecognized != nil {
+		n += len(m.XXX_unrecognized)
+	}
+	return n
+}
+
+func (m *HelloResp) Size() (n int) {
 	if m == nil {
 		return 0
 	}
@@ -288,6 +328,57 @@ func (m *HelloReq) Unmarshal(dAtA []byte) error {
 		}
 		if fieldNum <= 0 {
 			return fmt.Errorf("proto: HelloReq: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		default:
+			iNdEx = preIndex
+			skippy, err := skipApi(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthApi
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.XXX_unrecognized = append(m.XXX_unrecognized, dAtA[iNdEx:iNdEx+skippy]...)
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *HelloResp) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowApi
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: HelloResp: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: HelloResp: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
 		default:
