93ffe354e947036e35456cc205cee49046216089
zhangjingbohao <zhangjingbohao123@163.com>
2021-09-26 22:23:01 +0800

    update

--- fourth/api/api.bm.go
+++ fourth/api/api.bm.go
@@ -9,33 +9,20 @@ import (
 	bm "github.com/go-kratos/kratos/pkg/net/http/blademaster"
 	"github.com/go-kratos/kratos/pkg/net/http/blademaster/binding"
 )
-import google_protobuf1 "google.golang.org/protobuf/types/known/emptypb"
 
 // to suppressed 'imported but not used warning'
 var _ *bm.Context
 var _ context.Context
 var _ binding.StructValidator
 
-var PathDemoPing = "/demo.v1.Demo/Ping"
 var PathDemoSayHello = "/demo.v1.Demo/SayHello"
 
 // DemoBMServer is the server API for Demo service.
 type DemoBMServer interface {
-	Ping(ctx context.Context, req *google_protobuf1.Empty) (resp *google_protobuf1.Empty, err error)
-
-	SayHello(ctx context.Context, req *HelloReq) (resp *google_protobuf1.Empty, err error)
+	SayHello(ctx context.Context, req *HelloReq) (resp *HelloResp, err error)
 }
 
 var DemoSvc DemoBMServer
-
-func demoPing(c *bm.Context) {
-	p := new(google_protobuf1.Empty)
-	if err := c.BindWith(p, binding.Default(c.Request.Method, c.Request.Header.Get("Content-Type"))); err != nil {
-		return
-	}
-	resp, err := DemoSvc.Ping(c, p)
-	c.JSON(resp, err)
-}
 
 func demoSayHello(c *bm.Context) {
 	p := new(HelloReq)
@@ -49,6 +36,5 @@ func demoSayHello(c *bm.Context) {
 // RegisterDemoBMServer Register the blademaster route
 func RegisterDemoBMServer(e *bm.Engine, server DemoBMServer) {
 	DemoSvc = server
-	e.GET("/demo.v1.Demo/Ping", demoPing)
 	e.GET("/demo.v1.Demo/SayHello", demoSayHello)
 }
